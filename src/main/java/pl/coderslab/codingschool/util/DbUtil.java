ec01f001218454e3a8a58dd1c5bedd4d787c055c
Magda Stepien <magda.stepien1@gmail.com>
2019-03-02 13:00:48 +0100

    DbUtil file committed

---
+++ src/main/java/pl/coderslab/codingschool/util/DbUtil.java
@@ -0,0 +1,34 @@
+
+package pl.coderslab.codingschool.util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DbUtil {
+    private static final String CTX_NAME = "java:comp/env/jdbc/codingschool";
+
+    private static DataSource ds;
+
+    public static Connection getConn() throws SQLException, ClassNotFoundException {
+        String className = "com.mysql.cj.jdbc.Driver";
+        Class.forName(className);
+        return getInstance().getConnection();
+    }
+
+    private static DataSource getInstance() {
+        if (ds == null) {
+            try {
+                Context ctx = new InitialContext();
+                ds = (DataSource) ctx.lookup(CTX_NAME);
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+        return ds;
+    }
+}
+
