1dbf06c5f2f9692c4d7230c13f41acf1903411f7
Magda Stepien <magda.stepien1@gmail.com>
2019-03-02 13:08:43 +0100

    pom.xml file committed

---
+++ src/main/java/pl/coderslab/codingschool/model/User.java
@@ -0,0 +1,13 @@
+package pl.coderslab.codingschool.model;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User (String username, String email, String password) {
+
+    }
+}
